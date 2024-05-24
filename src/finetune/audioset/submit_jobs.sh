c85f9028ec139eb9c888c2ca58f92e7a2108f760
Siavash <siavashshams77@yahoo.com>
2024-05-23 22:44:32 -0400

    Update submit_jobs.sh

--- src/finetune/audioset/submit_jobs.sh
+++ src/finetune/audioset/submit_jobs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Array of pre-trained models
-declare -a models=("ssast_tiny_400")
+declare -a models=("ssamba_tiny_400")
 
 # Submit a job for each model
 for model in "${models[@]}"; do
-    sbatch run_as.sh $model
+    sbatch run_as_amba.sh $model
 done
 
